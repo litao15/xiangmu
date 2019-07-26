4f3f728300429cad4a4938e89b2220a9e1024fb6
1714145080@qq.com <1714145080@qq.com>
2019-07-26 10:10:38 +0800

    第一次

---
+++ src/main/java/com/czxy/hello.java
@@ -0,0 +1,11 @@
+package com.czxy;
+
+/**
+ * @version lt 1.0
+ * @date 2019/7/26
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
+}
