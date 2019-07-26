f36dfa0cd75bc4e60770f0f2281fca8a184d3d6c
17354639583 <53321687+17354639583@users.noreply.github.com>
2019-07-26 10:45:55 +0800

    Update hello.java

--- src/main/java/com/czxy/hello.java
+++ src/main/java/com/czxy/hello.java
@@ -7,5 +7,9 @@ package com.czxy;
 public class hello {
     public static void main(String[] args) {
         System.out.println("哈哈哈");
+        
+          System.out.println("malu");
+        
+        
     }
 }
